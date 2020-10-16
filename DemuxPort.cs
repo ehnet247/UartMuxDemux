ada9c57d21bfac2418659733b16e1df7b7cb8cb2
Etienne Hurand <ehurand@deltadore.com>
2020-10-16 16:02:11 +0200

    Create settings to save config

---
+++ DemuxPort.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UartMuxDemux
+{
+    public class DemuxPort
+    {
+        public SerialPort serialPort;
+        public string linkType;
+
+        public DemuxPort()
+        {
+            this.serialPort = new SerialPort();
+        }
+    }
+}
