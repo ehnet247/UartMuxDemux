21fb6f6e5c8ad3a3331a6246d119937a5694f95c
Etienne Hurand <informatique@etiennehurand.fr>
2020-10-17 15:35:48 +0200

    Open, close sort
    Add open and close com ports methods
    Sort com ports list in config form

--- MuxPort.cs
+++ MuxPort.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace UartMuxDemux
 {
-    class MuxPort
+    public class MuxPort
     {
+        public SerialPort serialPort;
+        public string linkType;
+
+        public MuxPort()
+        {
+            this.serialPort = new SerialPort();
+        }
     }
 }
