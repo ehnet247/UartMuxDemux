aa4b0ca101dc9239cb23da61b27ae1d08de9ce82
Etienne Hurand <informatique@etiennehurand.fr>
2020-10-19 12:05:06 +0200

    Change icon

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -32,20 +32,15 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.baudrateComboBox = new System.Windows.Forms.ToolStripComboBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.comboBoxBaudrate = new System.Windows.Forms.ComboBox();
             this.timerDisplayRefresh = new System.Windows.Forms.Timer(this.components);
             this.groupBoxLogger = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.comPortNameSource = new System.Windows.Forms.ComboBox();
+            this.checkedListBoxDemuxPorts = new System.Windows.Forms.CheckedListBox();
             this.serialPortSource = new System.IO.Ports.SerialPort(this.components);
             this.backgroundWorkerSource = new System.ComponentModel.BackgroundWorker();
             this.button1 = new System.Windows.Forms.Button();
             this.buttonOpenPorts = new System.Windows.Forms.Button();
             this.buttonClosePorts = new System.Windows.Forms.Button();
-            this.checkedListBoxDemuxPorts = new System.Windows.Forms.CheckedListBox();
             this.groupBoxLogger.SuspendLayout();
-            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // toolStrip1
@@ -53,7 +48,7 @@
             this.toolStrip1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
             this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.toolStrip1.Location = new System.Drawing.Point(420, -470);
+            this.toolStrip1.Location = new System.Drawing.Point(176, -470);
             this.toolStrip1.Name = "toolStrip1";
             this.toolStrip1.Size = new System.Drawing.Size(112, 25);
             this.toolStrip1.TabIndex = 12;
@@ -68,35 +63,6 @@
             this.baudrateComboBox.Name = "baudrateComboBox";
             this.baudrateComboBox.Size = new System.Drawing.Size(121, 28);
             // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(7, 24);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(70, 17);
-            this.label5.TabIndex = 29;
-            this.label5.Text = "Baudrate:";
-            // 
-            // comboBoxBaudrate
-            // 
-            this.comboBoxBaudrate.FormattingEnabled = true;
-            this.comboBoxBaudrate.Items.AddRange(new object[] {
-            "2400",
-            "4800",
-            "9600",
-            "14400",
-            "19200",
-            "38400",
-            "57600",
-            "115200",
-            "128000",
-            "256000"});
-            this.comboBoxBaudrate.Location = new System.Drawing.Point(83, 21);
-            this.comboBoxBaudrate.Name = "comboBoxBaudrate";
-            this.comboBoxBaudrate.Size = new System.Drawing.Size(98, 24);
-            this.comboBoxBaudrate.TabIndex = 28;
-            this.comboBoxBaudrate.SelectedIndexChanged += new System.EventHandler(this.comboBoxBaudrate_SelectedIndexChanged);
-            // 
             // timerDisplayRefresh
             // 
             this.timerDisplayRefresh.Tick += new System.EventHandler(this.timerDisplayRefresh_Tick_);
@@ -104,32 +70,20 @@
             // groupBoxLogger
             // 
             this.groupBoxLogger.Controls.Add(this.checkedListBoxDemuxPorts);
-            this.groupBoxLogger.Location = new System.Drawing.Point(364, 82);
+            this.groupBoxLogger.Location = new System.Drawing.Point(12, 12);
             this.groupBoxLogger.Name = "groupBoxLogger";
             this.groupBoxLogger.Size = new System.Drawing.Size(346, 302);
             this.groupBoxLogger.TabIndex = 23;
             this.groupBoxLogger.TabStop = false;
             this.groupBoxLogger.Text = "DEMUX";
             // 
-            // groupBox2
+            // checkedListBoxDemuxPorts
             // 
-            this.groupBox2.Controls.Add(this.label5);
-            this.groupBox2.Controls.Add(this.comboBoxBaudrate);
-            this.groupBox2.Controls.Add(this.comPortNameSource);
-            this.groupBox2.Location = new System.Drawing.Point(12, 82);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(346, 101);
-            this.groupBox2.TabIndex = 24;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "MUX";
-            // 
-            // comPortNameSource
-            // 
-            this.comPortNameSource.FormattingEnabled = true;
-            this.comPortNameSource.Location = new System.Drawing.Point(10, 51);
-            this.comPortNameSource.Name = "comPortNameSource";
-            this.comPortNameSource.Size = new System.Drawing.Size(121, 24);
-            this.comPortNameSource.TabIndex = 1;
+            this.checkedListBoxDemuxPorts.FormattingEnabled = true;
+            this.checkedListBoxDemuxPorts.Location = new System.Drawing.Point(6, 21);
+            this.checkedListBoxDemuxPorts.Name = "checkedListBoxDemuxPorts";
+            this.checkedListBoxDemuxPorts.Size = new System.Drawing.Size(323, 276);
+            this.checkedListBoxDemuxPorts.TabIndex = 0;
             // 
             // backgroundWorkerSource
             // 
@@ -137,7 +91,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(47, 189);
+            this.button1.Location = new System.Drawing.Point(16, 320);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(73, 29);
             this.button1.TabIndex = 26;
@@ -148,7 +102,7 @@
             // buttonOpenPorts
             // 
             this.buttonOpenPorts.BackColor = System.Drawing.Color.LimeGreen;
-            this.buttonOpenPorts.Location = new System.Drawing.Point(126, 189);
+            this.buttonOpenPorts.Location = new System.Drawing.Point(95, 320);
             this.buttonOpenPorts.Name = "buttonOpenPorts";
             this.buttonOpenPorts.Size = new System.Drawing.Size(113, 29);
             this.buttonOpenPorts.TabIndex = 27;
@@ -159,7 +113,7 @@
             // buttonClosePorts
             // 
             this.buttonClosePorts.BackColor = System.Drawing.Color.Red;
-            this.buttonClosePorts.Location = new System.Drawing.Point(245, 189);
+            this.buttonClosePorts.Location = new System.Drawing.Point(214, 320);
             this.buttonClosePorts.Name = "buttonClosePorts";
             this.buttonClosePorts.Size = new System.Drawing.Size(113, 29);
             this.buttonClosePorts.TabIndex = 28;
@@ -167,24 +121,15 @@
             this.buttonClosePorts.UseVisualStyleBackColor = false;
             this.buttonClosePorts.Click += new System.EventHandler(this.buttonClosePorts_Click);
             // 
-            // checkedListBoxDemuxPorts
-            // 
-            this.checkedListBoxDemuxPorts.FormattingEnabled = true;
-            this.checkedListBoxDemuxPorts.Location = new System.Drawing.Point(6, 21);
-            this.checkedListBoxDemuxPorts.Name = "checkedListBoxDemuxPorts";
-            this.checkedListBoxDemuxPorts.Size = new System.Drawing.Size(323, 276);
-            this.checkedListBoxDemuxPorts.TabIndex = 0;
-            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
-            this.ClientSize = new System.Drawing.Size(876, 418);
+            this.ClientSize = new System.Drawing.Size(388, 418);
             this.Controls.Add(this.buttonClosePorts);
             this.Controls.Add(this.buttonOpenPorts);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBoxLogger);
             this.Controls.Add(this.toolStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -192,8 +137,6 @@
             this.Text = "UartMuxDemux";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBoxLogger.ResumeLayout(false);
-            this.groupBox2.ResumeLayout(false);
-            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -202,12 +145,8 @@
         #endregion
         private System.Windows.Forms.ToolStrip toolStrip1;
         private System.Windows.Forms.ToolStripComboBox baudrateComboBox;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.ComboBox comboBoxBaudrate;
         private System.Windows.Forms.Timer timerDisplayRefresh;
         private System.Windows.Forms.GroupBox groupBoxLogger;
-        private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.ComboBox comPortNameSource;
         private System.IO.Ports.SerialPort serialPortSource;
         private System.ComponentModel.BackgroundWorker backgroundWorkerSource;
         private System.Windows.Forms.Button button1;
